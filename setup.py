d052c1ae40d9edcaf5b17e5aae11cb0eb0704f14
Basanth <basanthsk@gmail.com>
2021-09-18 19:52:44 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version="0.0.1",
+    description="its a diabetes package",
+    author=" Basanth",
+    packages=find_packages(),
+    license="MIT"
+)
